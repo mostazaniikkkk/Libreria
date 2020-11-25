a9238a5403419ea660d5557bd64df673327b3640
Sergio Valle <mostazaniikkkk@users.noreply.github.com>
2020-11-24 23:34:35 -0300

    Initial commit

---
+++ Libreria/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Libreria.AuxCategoria;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Libreria
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod(){
+            int id = 1;
+
+            int resultado = AuxCategoria;
+        }
+    }
+}
---
+++ Libreria/PruebaUnitaria00.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Libreria;
+using Microsoft;
+
+namespace Libreria
+{
+    public class UnitTest1
+    {
+        public void TestMethod()
+        {
+            int id = 1;
+
+            int resultado = AuxCategoria.Buscar(id);
+        }
+    }
+}
